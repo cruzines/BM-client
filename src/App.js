63e95a930d9f933ef81d78058e596c00e2a6669f
Ines Cruz <inesmncruz@gmail.com>
2021-12-09 12:16:02 +0000

    signup

--- src/App.js
+++ src/App.js
@@ -3,19 +3,34 @@ import { Routes, Route } from "react-router-dom";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 import axios from "axios";
+import { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import ModalDialog from "./components/ModalDialog";
 
 
 function App() {
 
-  
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div>
+      <div className="Auth">
+      <Button variant="contained" color="primary" onClick={handleOpen}>Signup</Button>
+      <ModalDialog open={open} handleClose={handleClose} />
+      </div>
     {/*  <MyNav onLogout={handleLogout} user={user} /> */}
     <h1>hello people :)</h1>
-     {/*  <Routes>
-        <Route  path="/signin" element={<SignIn myError={myError} onSignIn={handleSignIn} />}/>
-        <Route  path="/signup" element={<SignUp />}/>
-      </Routes> */}
+      <Routes>
+   
+      </Routes>
     </div>
   );
 }
