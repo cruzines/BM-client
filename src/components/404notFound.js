2be0c385beb601a1a887ea6f0fc01489d61a95bd
Maria Farran Colell <mafacoll@hotmail.com>
2021-12-09 19:44:00 +0100

    404 almost done

--- src/components/404notFound.js
+++ src/components/404notFound.js
@@ -9,7 +9,7 @@ function PageNotFound() {
               width: '100vw',
               height: '96vh',
               marginTop: -58 }}>
-        <h1 style={{ color: 'white', textAlign: 'center',}}>God created the world in six days and he's now resting. <br /> Go home.</h1>
+        <h1 style={{ color: 'white', textAlign: 'center', marginBottom: -30}}> 404 NOT FOUND<br />  God created the world in six days and he's now resting. <br /> Go home.</h1>
 
         </div>
     )
