408f89f6d001bf96efdcac02e2cef04c5bf12965
Ricardo Nunes <ricardo.agn.nunes@gmail.com>
2021-12-09 15:29:40 +0000

    refreshing the client

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ import ArtListing from "./components/ArtListing";
 import {useState, useEffect} from 'react';
 import {API_URL} from './config'
 import AddArt from "./components/AddArt";
+import MyNav from "./components/MyNav";
 
 function App() {
 
 
-  {/*
+  
   const [art, setArt] = useState([])
 
   useEffect(() => {
@@ -26,35 +27,12 @@ function App() {
 
 }, [])
 
-
-const handleSubmit = async (event, id) => {
-  event.preventDefault()
-  let newSale = {
-    artist: event.target.artist.value,
-    title: event.target.title.value,
-    year: event.targuet.year.value,
-    image: event.target.image.value,
-    price: event.targuet.image.value
-  }
-  console.log(newSale)
-  let response = await axios.post(`${API_URL}/sellform/${id}`, newSale)
-  setArt([response.data, ...art])
-}
-
-
-*/}
   
   return (
     <div>
-      {/* <AddArt /> 
-      <ArtListing /> 
-    <MyNav onLogout={handleLogout} user={user} /> */}
-    <h1>hello people :</h1>
-{/*
-      <Routes>
-      <Route path="/add-form" element={<AddArt btnSubmit={handleSubmit}/> } />
-      </Routes> 
-*/}
+      <MyNav />
+       <AddArt /> 
+      
     </div>
   );
 }
