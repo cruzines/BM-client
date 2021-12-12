65f5e270f15d52285cd2091ccc77a6400e0d6396
Maria Farran Colell <mafacoll@hotmail.com>
2021-12-12 19:59:50 +0100

    changes what changes

--- src/components/ArtListing.js
+++ src/components/ArtListing.js
@@ -4,57 +4,39 @@ import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
 import { Card } from 'antd';
 import '../Galery.css'
-import {Container, Row, Col } from 'react-bootstrap'
+
 
 const { Meta } = Card;
 
 function ArtListing(props) {
 //console.log(props)
-
     const {art} = props
 console.log(art)
-
-
-
     if(!art.length) {
         return <Box sx={{ display: 'flex' }}>
         <CircularProgress />
       </Box>
     }
-
     return (
-       
              <div className="containerX">
-            <Container fluid="md">
-            <Row>             
-            
             {
                 art.map((elem) => {
                     return (
-
-                            <Col className="colX">
-
-                            <Card hoverable 
+                            <div className="colX">
+                            <Link to={`/auctiondetail/${elem._id}`}>
+                            <Card hoverable
                             className='images'
                             style={{ width: 240 }}
-                            cover={<img alt="art" src={elem.image} />}>
-                            <Link to={`/auctiondetail/${elem._id}`}> <Meta title={elem.title}  /></Link>
+                            cover={<img alt="art" src={elem.image} height={200}/>}>
+                           <Meta title={elem.title}  />
+                           <p>by : {elem.artist}</p>
                             </Card>,
- 
-                            </Col>
-                          
-
-
-
+                            </Link>
+                            </div>
                     )
                 })
             }
-
-</Row>
-</Container>
 </div>
-        
     )
 }
-
 export default ArtListing
