687cae1935c1363ef8cdcb5813bcd27dbba32c59
Maria Farran Colell <mafacoll@hotmail.com>
2021-12-15 12:25:58 +0100

    press

--- src/components/Press.js
+++ src/components/Press.js
@@ -29,28 +29,22 @@ function Press() {
             <h2 class= 'lettering'>Bid Masters' international communications network is composed of very experienced press offices in more than 25 countries. They routinely  update the most important newspapers and media outlets of each country regarding future auctions. For more information we have the following social media presence: </h2>
             <p class= 'links'><a href="https://www.instagram.com/europe_treasures/">Instagram</a>|<a href="https://www.facebook.com/Bardia.Painting/">Facebook</a>|<a href="https://twitter.com/archaeologyart">Twitter</a>|<a href="https://www.youtube.com/c/BaumgartnerRestoration">Youtube</a>|<a href="https://www.pinterest.com/bcr8tive/art-paintings/">Pinterest</a></p>
             <h2 class= 'lettering'> If you want to contact with us: <br/></h2>
-
-
+         
+<div class= 'card' > 
             <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         component="img"
         height="140"
-        image="/static/images/cards/contemplative-reptile.jpg"
-        alt="green iguana"
+        image="../monet.jpg"
+        alt="Ava  Müller"
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Lizard
+        Ava  Müller 
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
+        Senior Vice President | Chief Communications Officer, Europe - avamuller@bidmasters.com</Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
     </Card>
 
     
@@ -58,28 +52,20 @@ function Press() {
       <CardMedia
         component="img"
         height="140"
-        image="/static/images/cards/contemplative-reptile.jpg"
-        alt="green iguana"
+        image="../dali.jpg"
+        alt="Albert  Martin"
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Lizard
+        Albert  Martin
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
+        Vice President | Senior Press Officer, North America   - albertmartin@bidmasters.com    </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
     </Card>
+    </div>
 
-            <h2 class= 'lettering'> Albert  Martin: Vice President | Senior Press Officer, North America   - albertmartin@bidmasters.com<br/></h2>
-            <h2 class= 'lettering'> Ava  Müller: Senior Vice President | Chief Communications Officer, Europe - avamuller@bidmasters.com<br/></h2>
-
-
+¡
 
             <div className="errorSize">
         <h1 style={{ color: '#04435D', textAlign: 'center', marginBottom: -62}}> </h1>
