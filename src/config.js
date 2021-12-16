ccb9e20de2dd5d4de7b3cef69315b355be371dc1
Ines Cruz <inesmncruz@gmail.com>
2021-12-16 20:09:56 +0000

    deploy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5005/api'
-//const API_URL = 'https://bidmasters.herokuapp.com/api'
+//const API_URL = 'http://localhost:5005/api'
+const API_URL = 'https://bidmasters.herokuapp.com/api'
 
 export {API_URL}
